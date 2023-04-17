0ebd2cc7cb12e2001dfe6050bbf786307db4adfb
Creative Lion <86979388+AndersonCreativeLion@users.noreply.github.com>
2023-04-17 10:01:31 -0300

    UI Decathon

---
+++ Assets/Scripts Anderson/ChangeScene.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+
+	public void ChangeToScene (string sceneToChangeTo) {
+		
+		Application.LoadLevel(sceneToChangeTo);
+		
+	}
+
+
+
+}
+	
+	
